ec084c0e6b6d07c27e2b3be7880522470eeafe29
Alok Jha <alok08jha@gmail.com>
2022-07-20 12:07:55 +0530

    databse connection
    Signed-off-by: Alok Jha <alok08jha@gmail.com>

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -13,34 +13,28 @@ import (
 )
 
 func DBinstance() *mongo.Client {
-
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	MongoDB := os.Getenv("MONGODB_URL")
-
-	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	MongoDb := os.Getenv("MONGODB_URL")
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	err = client.Connect(ctx)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Connected to MONGODB")
+	fmt.Println("Connected to MongoDB!")
 	return client
 }
 
 var Client *mongo.Client = DBinstance()
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	var Collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
-	return Collection
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	return collection
 }
